fce552e8048dd7761de4e6f08834a5b1189e099f
Robert Ing <robertming@gmail.com>
2013-12-11 17:29:18 -0800

    first commit

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+
+gem 'rails', '4.0.0'
+ruby '2.0.0'
+
+group :development do
+  gem 'sqlite3'
+end
+
+
+group :production do
+  gem 'pg'
+end
+
+
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fitnessfriendfinder::Application.config.secret_key_base = 'dd66c997eed750314088728b24c520288a0f2cba6c8cd7dded89b9c5614cc915877aa44af951540f5fc57fdc883ad18bdae4786ac24f76f9adc44d55bc06c3d6'
